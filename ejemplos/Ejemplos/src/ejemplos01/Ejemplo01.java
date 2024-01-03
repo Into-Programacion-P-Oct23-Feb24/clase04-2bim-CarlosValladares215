14dcf744c2317e35addc7f791dce70545ac78bf9
Carlos Valladares <csvalladares2@utpl.edu.ec>
2024-01-03 16:06:40 -0500

    Ejemplo 01

--- ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
+++ ejemplos/Ejemplos/src/ejemplos01/Ejemplo01.java
@@ -17,19 +17,47 @@ public class Ejemplo01 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        // String miCiudad = obtenerCiudadMayuscula("Loja");
+        // System.out.printf("%s\n", miCiudad);
         
-        String miCiudad = obtenerCiudadMayuscula("Loja");
-        System.out.printf("%s\n", miCiudad);
+        // se invoca a obtenerMultipliacion poeque es un procedimiento
+        // y no devuelve ningun valor
+        obtenerMultiplicacion(7,50);
+        
+        // En este caso obtenerMultiplicacion es una funcion que devuelve un
+        // valor tipo String, es por eso que que el valor devuelto es recogido
+        // por la variable tipo cadena miMensaje
+        String miMensaje = obtenerMultiplicacionDos(7,50);
+        System.out.printf("%s\n",miMensaje);
     }
     
-    
-    public String obtenerCiudadMayuscula(String m){
+    // Por ahora para crear funciones deberan de tener esta estructura
+    public static String obtenerCiudadMayuscula(String m){
         String m2 = m.toUpperCase();
         return m2;
     }
     
+    // Generar una tabla del 7 que empieze desde 1 y vaya a 100, con acumulador
     public static void obtenerMultiplicacion(int tabla, int limite){
-        
+        int res;
+        String tablaDel7 = "";
+        for(int i = 1; i<=limite;i++){
+            res = tabla * i;
+            tablaDel7 = String.format("%s%d * %d = %d\n",tablaDel7
+                    , tabla,i,res);
+        }
+        System.out.printf("%s",tablaDel7);
+    }
+    
+    public static String obtenerMultiplicacionDos(int tabla, int limite){
+        int res;
+        String tablaDel_7 = "";
+        for(int i = 1; i<=limite;i++){
+            res = tabla * i;
+            tablaDel_7 = String.format("%s%d * %d = %d\n",tablaDel_7
+                    , tabla,i,res);
+        }
+        return tablaDel_7;
     }
     
     public static String obtenerNombre(){
